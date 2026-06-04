7afdd1c07fff3c40f530d8e97baab26f9b4d6a9d
Samarth Jadhav <samarthjadhav1033@gmail.com>
2026-06-04 11:01:20 +0530

    initial commit

---
+++ var.js
@@ -0,0 +1,26 @@
+// var a="hello";
+// var a=20;
+// console.log(a); 
+// redeclaration is possible
+
+// var a="hello";
+//  a=20;
+// console.log(a);
+// reassinment is possible
+
+// a=20;
+// var a;
+// console.log(a);
+// hoisting is possible
+
+// let a=Number(prompt("Enter first number"));
+// let b=Number(prompt("Enter second number"));
+// let c=Number(prompt("Enter third number"));
+// let a=11
+// let b=20
+// let c=15
+// let midd= ((a>b&&a<c) ||(a<b&&a>c))? a:((b>a&&b<c)||(b<a&&b>c))? b : c;
+// 
+// console.log("middle number is",midd);
+
+
